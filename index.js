934894c91ffbea4b000e4745b11b3add0fdc61bc
Rahul <vermarahulkps@gmail.com>
2018-07-10 12:45:45 +0530

    only warnings left

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
  * @param {Array} items
  * @returns {Array}
  */
-const removeDuplicateItems = (items) => {};
+const removeDuplicateItems = (items) => {
+  let i = 0;
+  const set = new Set();
+  for (i = 0; i < items.length; i += 1) {
+    set.add(items[i]);
+  }
+  return Array.from(set);
+};
 
 module.exports = removeDuplicateItems;
