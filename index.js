70169da461942b7f5041cdb8d0f532a7316b26ca
Rahul <vermarahulkps@gmail.com>
2018-07-14 21:33:58 +0530

    replaced with es6 function and did Array.from(new Set(items))

--- index.js
+++ index.js
@@ -3,13 +3,12 @@
  * @param {Array} items
  * @returns {Array}
  */
-const removeDuplicateItems = (items) => {
-  let i = 0;
-  const set = new Set();
-  for (i = 0; i < items.length; i += 1) {
-    set.add(items[i]);
-  }
-  return Array.from(set);
-};
+const removeDuplicateItems = items => Array.from(new Set(items));
+// const set = new Set();
+// for (let i = 0; i < items.length; i += 1) {
+//   set.add(items[i]);
+// }
+// return Array.from(set);
+
 
 module.exports = removeDuplicateItems;
